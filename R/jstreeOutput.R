2add9789eb48538ef13bbc926ed0e83749bac1a8
nomealeatorio999 <nomealeatorio999@gmail.com>
2017-06-29 18:19:19 -0400

    make search as shiny-input
    add minor css modifications

--- R/jstreeOutput.R
+++ R/jstreeOutput.R
@@ -1,31 +1,35 @@
 #' @export
 jstreeOutput <- function(outputId, checkbox=FALSE, search=FALSE, search_placeholder = ""){
-    tag_search <- shiny::tags$div("")
+
+    searchEl <- shiny::div("")
     if (search == TRUE){
         search <- paste0(outputId, "-search-input")
-        tag_search <- shiny::tags$input(id=search, class="input",
-                                        type="text", value="", placeholder = search_placeholder)
+        searchEl <- shiny::tags$input(id=search, class="form-control", type="text", value="")
     }
     if (is.character(search)){
         # Either the search field we just created or the given text field ID
-        tag_search <- shiny::tagAppendChild(
-            tag_search,
-            shiny::tags$script(
-                type="text/javascript",
-                shiny::HTML(paste0("jstreeFun.initSearch('",outputId,"','",search,"');"))))
+        searchEl <- shiny::tagAppendChild(searchEl, shiny::tags$script(type="text/javascript", shiny::HTML(
+            paste0("jstreeFun.initSearch('",outputId,"','",search,"');"))))
     }
+
+    searchEl <- shiny::tags$div(
+        class="form-group shiny-input-container",
+        searchEl
+    )
 
     shiny::tagList(
         shiny::singleton(shiny::tags$head(
             initResourcePaths(),
             shiny::tags$link(rel = 'stylesheet', type = 'text/css',
                              href = 'jstree/jsTree-3.3.4/themes/default/style.min.css'),
+            shiny::tags$link(rel = 'stylesheet', type = 'text/css',
+                             href = 'jstree/jstree.css'),
             shiny::tags$link(rel = "stylesheet", type = "text/css",
                              href = "shared/font-awesome/css/font-awesome.min.css"),
             shiny::tags$script(src = 'jstree/jsTree-3.3.4/jstree.min.js'),
             shiny::tags$script(src = 'jstree/jstree.js')
         )),
-        tag_search,
+        searchEl,
         shiny::div(id=outputId, class = "shiny-jstree",
                    `data-st-checkbox` = checkbox,
                    `data-st-search` = is.character(search))
